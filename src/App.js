497ac30ca20d2a02e4f6b772b5c57491e9cd03b5
Aaryan121 <aaryan.cs01@gmail.com>
2022-11-04 09:47:20 +0545

    trying out api handling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 import Counter from "./features/Counter";
 
+
+
+
 function App() {
+
+  useEffect(()=>{
+    const api = 'https://pokeapi.co/api/v2/pokemon/ditto'
+
+    axios(api).then((response) =>{
+      console.log(response.data)
+    });
+
+  },[])
+
+
   return (
 
     <Counter />
